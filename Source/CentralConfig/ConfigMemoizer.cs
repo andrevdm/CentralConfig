0d675eabe9ddf2fefdd951c52cc788ea7aade088
Andre vd Merwe <andrevdm@pcubed.jhb>
2016-05-20 14:28:46 +0200

    Enable memoization and manual config

---
+++ Source/CentralConfig/ConfigMemoizer.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Concurrent;
+
+namespace CentralConfig
+{
+    public class ConfigMemoizer : IConfigPersistor
+    {
+        private readonly bool m_enable;
+        private readonly IConfigPersistor m_configPersistor;
+        private readonly ConcurrentDictionary<Tuple<string, string, long>, string> m_values = new ConcurrentDictionary<Tuple<string, string, long>, string>();
+        private readonly ConcurrentDictionary<Tuple<string, string, long>, object> m_sections = new ConcurrentDictionary<Tuple<string, string, long>, object>();
+
+        public ConfigMemoizer( bool enable, IConfigPersistor configPersistor )
+        {
+            if( configPersistor == null )
+                throw new ArgumentNullException( nameof( configPersistor ) );
+
+            m_enable = enable;
+            m_configPersistor = configPersistor;
+        }
+
+        private string ReadAppSetting( Tuple<string, string, long> key, Func<string> read )
+        {
+            if( !m_enable )
+                return read();
+
+            string value;
+            if( m_values.TryGetValue( key, out value ) )
+                return value;
+
+            value = read();
+            m_values[key] = value;
+            return value;
+        }
+
+        private object ReadSection( Tuple<string, string, long> key, Func<object> read )
+        {
+            if( !m_enable )
+                return read();
+
+            object value;
+            if( m_sections.TryGetValue( key, out value ) )
+                return value;
+
+            value = read();
+            m_sections[key] = value;
+            return value;
+        }
+
+        public string ReadAppSetting( string key, string machine, long version )
+        {
+            return ReadAppSetting( new Tuple<string, string, long>( key, machine, version ), () => m_configPersistor.ReadAppSetting( key, machine, version ) );
+        }
+
+        public string ReadAppSetting( string key, string machine )
+        {
+            return ReadAppSetting( new Tuple<string, string, long>( key, machine, long.MinValue ), () => m_configPersistor.ReadAppSetting( key, machine ) );
+        }
+
+        public string ReadAppSetting( string key, long version )
+        {
+            return ReadAppSetting( new Tuple<string, string, long>( key, "", version ), () => m_configPersistor.ReadAppSetting( key, version ) );
+        }
+
+        public string ReadAppSetting( string key )
+        {
+            return ReadAppSetting( new Tuple<string, string, long>( key, "", long.MinValue ), () => m_configPersistor.ReadAppSetting( key ) );
+        }
+
+        public TResult GetSection<TResult>( string sectionName, string machine, long version )
+        {
+            return (TResult)ReadSection( new Tuple<string, string, long>( sectionName, machine, version ), () => m_configPersistor.GetSection<TResult>( sectionName, machine, version ) );
+        }
+
+        public TResult GetSection<TResult>( string sectionName, string machine )
+        {
+            return (TResult)ReadSection( new Tuple<string, string, long>( sectionName, machine, long.MinValue ), () => m_configPersistor.GetSection<TResult>( sectionName, machine ) );
+        }
+
+        public TResult GetSection<TResult>( string sectionName, long version )
+        {
+            return (TResult)ReadSection( new Tuple<string, string, long>( sectionName, "", version ), () => m_configPersistor.GetSection<TResult>( sectionName, version ) );
+        }
+
+        public TResult GetSection<TResult>( string sectionName )
+        {
+            return (TResult)ReadSection( new Tuple<string, string, long>( sectionName, "", long.MinValue ), () => m_configPersistor.GetSection<TResult>( sectionName ) );
+        }
+    }
+}
--- Source/CentralConfig/MongoDbConfigPersistor.cs
+++ Source/CentralConfig/MongoDbConfigPersistor.cs
@@ -1,4 +1,5 @@
-﻿using System.Configuration;
+﻿using System;
+using System.Configuration;
 using MongoDB.Bson;
 using MongoDB.Driver;
 using MongoDB.Driver.Builders;
@@ -7,6 +8,84 @@ namespace CentralConfig
 {
     public class MongoDbConfigPersistor : IConfigPersistor
     {
+        private readonly ConfigMemoizer m_memoizer;
+
+        public MongoDbConfigPersistor( bool memoize = false )
+        {
+            var configPersistor = new MongoDbConfigPersistorImpl();
+            m_memoizer = new ConfigMemoizer( memoize, configPersistor );
+        }
+
+        public MongoDbConfigPersistor( string mongoConStr, string mongoDatabase, bool memoize = false )
+        {
+            var configPersistor = new MongoDbConfigPersistorImpl(mongoConStr, mongoDatabase);
+            m_memoizer = new ConfigMemoizer( memoize, configPersistor );
+        }
+
+        public string ReadAppSetting( string key, string machine, long version )
+        {
+            return m_memoizer.ReadAppSetting( key, machine, version );
+        }
+
+        public string ReadAppSetting( string key, string machine )
+        {
+            return m_memoizer.ReadAppSetting( key, machine );
+        }
+
+        public string ReadAppSetting( string key, long version )
+        {
+            return m_memoizer.ReadAppSetting( key, version );
+        }
+
+        public string ReadAppSetting( string key )
+        {
+            return m_memoizer.ReadAppSetting( key );
+        }
+
+        public TResult GetSection<TResult>( string sectionName, string machine, long version )
+        {
+            return m_memoizer.GetSection<TResult>( sectionName, machine, version );
+        }
+
+        public TResult GetSection<TResult>( string sectionName, string machine )
+        {
+            return m_memoizer.GetSection<TResult>( sectionName, machine );
+        }
+
+        public TResult GetSection<TResult>( string sectionName, long version )
+        {
+            return m_memoizer.GetSection<TResult>( sectionName, version );
+        }
+
+        public TResult GetSection<TResult>( string sectionName )
+        {
+            return m_memoizer.GetSection<TResult>( sectionName );
+        }
+    }
+
+    public class MongoDbConfigPersistorImpl : IConfigPersistor
+    {
+        private readonly string m_mongoConStr;
+        private readonly string m_mongoDatabase;
+
+        public MongoDbConfigPersistorImpl()
+        {
+            m_mongoConStr = ConfigurationManager.AppSettings["MongoDB.Server"];
+            m_mongoDatabase = ConfigurationManager.AppSettings["CentralConfig.MongoDatabase"];
+        }
+
+        public MongoDbConfigPersistorImpl( string mongoConStr, string mongoDatabase )
+        {
+            if( mongoConStr == null )
+                throw new ArgumentNullException( nameof( mongoConStr ) );
+
+            if( mongoDatabase == null )
+                throw new ArgumentNullException( nameof( mongoDatabase ) );
+
+            m_mongoConStr = mongoConStr;
+            m_mongoDatabase = mongoDatabase;
+        }
+
         public string ReadAppSetting( string key, string machine, long version )
         {
             var db = GetConfigDatabase();
@@ -18,7 +97,7 @@ namespace CentralConfig
                     Query.EQ( "machine", machine ),
                     Query.EQ( "version", version ) ) );
 
-            return result != null ? result.value : null;
+            return result?.value;
         }
 
         public string ReadAppSetting( string key, string machine )
@@ -32,7 +111,7 @@ namespace CentralConfig
                     Query.EQ( "machine", machine ),
                     Query.EQ( "version", BsonNull.Value ) ) );
 
-            return result != null ? result.value : null;
+            return result?.value;
         }
 
         public string ReadAppSetting( string key, long version )
@@ -46,7 +125,7 @@ namespace CentralConfig
                     Query.EQ( "machine", BsonNull.Value ),
                     Query.EQ( "version", version ) ) );
 
-            return result != null ? result.value : null;
+            return result?.value;
         }
 
         public string ReadAppSetting( string key )
@@ -60,7 +139,7 @@ namespace CentralConfig
                     Query.EQ( "machine", BsonNull.Value ),
                     Query.EQ( "version", BsonNull.Value ) ) );
 
-            return result != null ? result.value : null;
+            return result?.value;
         }
 
         public TResult GetSection<TResult>( string sectionName, string machine, long version )
@@ -74,7 +153,7 @@ namespace CentralConfig
                     Query.EQ( "machine", machine ),
                     Query.EQ( "version", version ) ) );
 
-            return result != null ? result.sectionData : default(TResult);
+            return result != null ? result.sectionData : default( TResult );
         }
 
         public TResult GetSection<TResult>( string sectionName, string machine )
@@ -121,10 +200,10 @@ namespace CentralConfig
 
         private MongoDatabase GetConfigDatabase()
         {
-            string con = ConfigurationManager.AppSettings["MongoDB.Server"];
+            string con = m_mongoConStr;
             var client = new MongoClient( con );
             var server = client.GetServer();
-            var db = server.GetDatabase( ConfigurationManager.AppSettings["CentralConfig.MongoDatabase"] );
+            var db = server.GetDatabase( m_mongoDatabase );
             return db;
         }
 
